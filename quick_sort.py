49737d6960e13c33a0c50dcc64c8b04c3063ab5c
Stefan Birsan <83671730+StefanBirsan@users.noreply.github.com>
2024-04-30 22:18:37 +0300

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,37 +1,38 @@
 import random
 import time
 
+
 def partition(arr, low, high):
 
-  pivot = arr[high]  
-  
-  i = low - 1  
+  pivot = arr[high]
+
+  i = low - 1
 
   for j in range(low, high):
-    
-    if arr[j] < pivot:
-     
-      i += 1
-     
-      arr[i], arr[j] = arr[j], arr[i]
 
+    if arr[j] < pivot:
+
+      i += 1
+
+      arr[i], arr[j] = arr[j], arr[i]
 
   arr[i + 1], arr[high] = arr[high], arr[i + 1]
 
   return i + 1
 
+
 def quickSort(arr, low, high):
 
   if low < high:
-   
-    pi = partition(arr, low, high)
 
+    pi = partition(arr, low, high)
 
     quickSort(arr, low, pi - 1)
     quickSort(arr, pi + 1, high)
 
 
-my_list = list(range(10))
+number = int(input())
+my_list = list(range(number))
 random.shuffle(my_list)
 
 print("Original list:", my_list)
